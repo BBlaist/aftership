f50464530892309d418693294cc1aea80a779a3b
Dirk Rijntalder <dirkrijntalder@gmail.com>
2025-04-16 14:45:50 +0200

    Header Demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="p-0 m-0">
-      <nav className="bg-gray-700 w-screen h-24 flex items-center p-4">
+      <nav className="bg-gray-700 w-screen h-24 flex items-center p-4"> 
         <Image 
           src="/aftership-logo2.png"
           alt="Logo"
           width={100}
           height={100}
-          className="rounded-full"
+          className=""
         />
-        <div className="w-full flex justify-evenly">
-        <a href="">one</a>
-        <a href="">two</a>
-        <a href="">three</a>
+        <div className="w-full flex justify-evenly text-2xl font-bold">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
         </div>
       </nav>
     </div>
